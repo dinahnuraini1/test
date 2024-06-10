2d9294590d5a12cd2d83f3920b5c76fdadbd2973
dinahnuraini <112395290+dinahnuraini1@users.noreply.github.com>
2024-06-10 21:11:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Unit Kesehatan | Daop 7 Madiun</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/logo1.png">
+
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/mainn.css" rel="stylesheet">
+  <style>
+    .fixed-size {
+      width: 100%;
+      height: 300px;
+      /* Set a fixed height */
+      object-fit: cover;
+      /* Ensures the image covers the entire area */
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/logo1.png" alt="" style="max-width: 200%; height:auto;">
+        <h1>Unit Kesehatan | Daop 7 Madiun </h1>
+      </a>
+      <!-- <i class=" mobile-nav-toggle mobile-nav-show bi bi-list"></i> -->
+      <!-- <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i> -->
+      <nav id="navbar" class="navbar" style="color:red">
+        <ul>
+          <li><a href="index.php" class="active">Home</a></li>
+          <!-- <li><a href="login.php">Login</a></li> -->
+          <li><a href="#gallery">gallery</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero">
+
+    <div class="info d-flex align-items-center">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-10 text-center">
+            <h3 style="color: white;" data-aos="fade-down">Selamat Datang di UK Madiun</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <div class="carousel-item active" style="background-image: url(assets/img/depan/6.jpeg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/7.jpeg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/8.jpeg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/9.jpeg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/10.jpeg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/2.jpg)"></div>
+      <div class="carousel-item " style="background-image: url(assets/img/depan/3.jpg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/4.jpg)"></div>
+      <div class="carousel-item" style="background-image: url(assets/img/depan/5.jpg)"></div>
+
+      <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+
+  </section><!-- End Hero Section -->
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+        <div class="section-menu text-center">
+          <h2>Menu Dashboard</h2>
+        </div>
+      </div>
+
+      <div class="row gy-4">
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+          <div class="service-item  position-relative" style="background-color:#F27BBD; border-radius: 20px;">
+            <div class="icon">
+              <i class="fa-solid"><img src="assets/img/icon/human.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Human Capital</h3>
+            <!-- <ul>
+              <li>Data & foto management</li>
+            </ul> -->
+            <a href="human-capital/coba.php" class="readmore stretched-link"></a>
+          </div>
+        </div><!-- End Service Item -->
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+          <div class="service-item position-relative" style="background-color: #FFEAA7; border-radius: 20px;">
+            <div class=" icon">
+              <i class="fa-solid"><img src="assets/img/icon/health.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Health Budget</h3>
+            <!-- <ul>
+              <li>Anggaran Commitment Item</li>
+              <li>Program, Realisasi dan Sisa Anggaran</li>
+              <li>Pendapatan</li>
+            </ul> -->
+            <a href="./health-budget/anggaran_percommitmenitem.php" class="readmore stretched-link"></a>
+          </div>
+        </div><!-- End Service Item -->
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+          <div class="service-item position-relative" style="background-color:#C6EBC5; border-radius: 20px;">
+            <div class="icon">
+              <i class="fa-solid"><img src="assets/img/icon/activity.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Activity Plan</h3>
+            <!-- <ul>
+              <li>Tabel Program Kerja</li> -->
+            <!-- <ul>
+                <li>Time Line</li>
+                <li>PIC</li>
+                <li>Status</li>
+              </ul> -->
+            </ul>
+            <a href="./activity/dashboard.php" class="readmore stretched-link"></i></a>
+          </div>
+        </div><!-- End Service Item -->
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+          <div class="service-item position-relative" style="background-color: #C3B9EA; border-radius: 20px;">
+            <div class="icon">
+              <i class="fa-solid"><img src="assets/img/icon/healthservices.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Health Services</h3>
+            <!-- <ul>
+              <li>Kunjungan Klinik</li>
+              <li>Jumlah Rujukan</li>
+              <li>Jumlah Tagihan</li>
+              <li>Klinik Mediska (foto, nama & alamat)</li>
+              <li>Faskes Kerja Sama (RS, klinik, apotek, optik, laboratorium & dokkel) </li>
+            </ul> -->
+            <a href="./health-services/kunjungan_klinik.php" class="readmore stretched-link"></i></a>
+          </div>
+        </div><!-- End Service Item -->
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+          <div class="service-item position-relative" style="background-color: #A3D8FF; border-radius: 20px;">
+            <div class="icon">
+              <i class="fa-solid"><img src="assets/img/icon/occupationalhealth.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Occupational Health</h3>
+            <!-- <ul>
+              <li>Hasil MCU</li>
+              <li>Hasil Test Kebugaran</li>
+              <li>Jumlah Rikes </li>
+              <ul>
+                <li>Awak KA</li>
+                <li>Setifikasi</li>
+                <li>Rikes Khusus</li>
+              </ul>
+            </ul> -->
+            <a href="./occupational/flyer.php" class="readmore stretched-link"></i></a>
+          </div>
+        </div><!-- End Service Item -->
+
+        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+          <div class="service-item position-relative" style="background-color: #FFBB70; border-radius: 20px;">
+            <div class="icon">
+              <i class="fa-solid"><img src="assets/img/icon/safety.png" alt="" width="60" height="60"></i>
+            </div>
+            <h3>Safety Action</h3>
+            <!-- <ul>
+              <li>IBPR (Identifikasi Bahaya dan Pengendalian Risiko)</li>
+              <li>Hasil supervisi alat kerja</li>
+              <li>Hasil Supervisi lingkungan</li>
+            </ul> -->
+            <a href="./safety-action/safety_action.php" class="readmore stretched-link"></i></a>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <?php
+    // Include file koneksi ke database
+    include 'koneksi.php';
+
+    // Query untuk mengambil data gambar dari database
+    $query = "SELECT * FROM tb_gambar";
+    $result = mysqli_query($conn, $query);
+    ?>
+
+    <section id="projects" class="projects">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header" id="gallery">
+          <h2>Gallery</h2>
+        </div>
+        <a href=" tambah_gambar.php">
+          <i class="fa-solid"><img src="assets/img/icon/plus.png" alt="" width="30" height="30"></i>
+        </a><br><br>
+        <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
+          <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+            <?php
+            // Loop untuk menampilkan gambar-gambar dalam galeri
+            while ($row = mysqli_fetch_assoc($result)) {
+
+
+            ?>
+              <!-- <img style="width:100px; height: 100px;" src="gambar_gallery/<?php echo $row['gambar']; ?>" /> -->
+              <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+                <div class="portfolio-content h-100">
+                  <img class="fixed-size" style="width:100%; height: 100;" src="gambar_gallery/<?php echo $row['gambar']; ?>" />
+                  <div class="portfolio-info">
+                    <a href="gambar_gallery/<?php echo $row['gambar']; ?>" title="Download Gambar" download><i class="bi bi-download"></i></a>
+
+                    <a href="gambar_gallery/<?php echo $row['gambar']; ?>" title="Kegiatan: <?php echo $row['nama_kegiatan']; ?><br> Tanggal: <?php echo date('d-m-Y', strtotime($row['tanggal'])); ?>" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                    <a href="#" onclick="confirmDelete(<?php echo $row['id']; ?>)" title="Hapus Gambar" class="delete-link"><i class="bi bi-trash"></i></a>
+                    <script>
+                      function confirmDelete(id) {
+                        var confirmDelete = confirm("Apakah Anda yakin ingin menghapus gambar?");
+                        if (confirmDelete) {
+                          window.location.href = "hapus_gambar.php?id=" + id;
+                        } else {
+                          // Kembali ke bagian galeri
+                          window.location.hash = "gallery";
+                        }
+                      }
+                    </script>
+                  </div>
+                </div>
+              </div><!-- End Projects Item -->
+
+              <!-- End Projects Item -->
+            <?php
+            }
+            ?>
+          </div><!-- End Projects Container -->
+        </div>
+      </div>
+    </section>
+    <!-- End Our Projects Section -->
+
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-content position-relative">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Unit Kesehatan</h3>
+              <p>
+                Kompol Sunaryo, Madiun Lor, <br>
+                Kec. Manguharjo, Kota Madiun, <br>
+                Jawa Timur 63122 <br><br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <!-- <a href="https://twitter.com/keretaapikita" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a> -->
+                <!-- <a href="https://www.facebook.com/keretaapikita" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a> -->
+                <!-- <a href="https://www.instagram.com/keretaapikita" class="d-flex align-items-center justify-content-center" target="blank;"><i class="bi bi-instagram"></i></a>
+                <a href="https://wa.me/6285604614755" class="d-flex align-items-center justify-content-center" target="blank;"><i class=" bi bi-whatsapp"></i> </a> -->
+                <!-- <a href="https://www.youtube.com/keretaapikita" class="d-flex align-items-center justify-content-center"><i class="bi bi-youtube"></i></a> -->
+              </div>
+            </div>
+          </div><!-- End footer info column-->
+
+          <div class="col-lg-8 col-md-1" style="text-align: justify; margin-top:15px; line-height:30px;">
+            <p class="text-justify">Unit Kesehatan Daop VII Madiun merupakah sebuah organisasi di bawah daerah Operasi VII Madiun.
+              yang wilayah kerjanya meliputi seluruh wilayah kerja Daop VII Madiun. Unit Kesehatan Daop VII Madiun
+              memberikan pelayanan kesehatan primer bagi pekerja dan keluarga serta memantau kesehatan pekerja
+              di lingkungan daop VII Madiun.</p>
+          </div><!-- End footer links column-->
+
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+      <div class="container">
+        <div class="copyright">
+          PT Kereta Api Indonesia (Persero) <strong><span>&copy;2024</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+
+</html>
